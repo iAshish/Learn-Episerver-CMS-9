b58eaa2e735221637c4dd4bb33281a124d66444c
Chougule_Parivar <Chougule_Parivar@CHOUGULEPARIVAR>
2017-09-24 11:14:56 +0530

    Add project files.

---
+++ LearnEpiserver/Controllers/StartPageController.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Web.Mvc;
+using LearnEpiserver.Models.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnEpiserver.Controllers
+{
+    public class StartPageController : PageController<StartPage>
+    {
+        // GET: StartPage
+        public ActionResult Index(StartPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
---
+++ LearnEpiserver/Models/Pages/StartPage.cs
@@ -0,0 +1,17 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnEpiserver.Models.Pages
+{
+    [ContentType]
+    public class StartPage : PageData
+    {
+        
+        public virtual string Heading { get; set; }
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
